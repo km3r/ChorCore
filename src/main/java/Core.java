6cd6d888c52c2d6092c8327d63c00411401a3268
Kyle Rosenthal <kylem3r@gmail.com>
2017-04-20 01:15:34 -0700

    gradle skeleton

---
+++ src/main/java/Core.java
@@ -0,0 +1,10 @@
+/**
+ * chorCore
+ * <p>
+ * Created by Kyle on 4/20/2017.
+ */
+public class Core {
+    public boolean foo() {
+        return true;
+    }
+}
---
+++ src/test/java/CoreTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+/**
+ * chorCore
+ * <p>
+ * Created by Kyle on 4/20/2017.
+ */
+public class CoreTest {
+
+    @Test
+    public void testTest(){
+        Core core = new Core();
+        assert core.foo();
+    }
+
+}
